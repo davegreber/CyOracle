7d0282db251c4a5447d0ebcf84d8674eaa114452
Dave Greber <davegreber@gmail.com>
2023-02-24 13:50:09 -0500

    working reset

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ const images = [
   {
     name: "tarot_cups_9.jpg",
     src: "/images/tarot_cups_9.jpg",
-    alt: "Cups 9",
-    caption: "This is the caption for Cups 9."
+    alt: "stone relief of vegetables",
+    title: "The 9 of Cups",
+    caption: "The Smart refrigerator with grocery ordering represents abundance, prosperity, and satisfaction in the emotional and relational aspects of life. This card may indicate that it's time to achieve abundance and satisfaction. Use the smart refrigerator with grocery ordering to ensure your fridge is always stocked and enjoy the abundance in your life."
   },
   {
     name: "tarot_cups_2.jpg",
     src: "/images/tarot_cups_2.jpg",
-    alt: "Cups 2",
-    caption: "This is the caption for Cups 2."
+    alt: "abstract stone rune",
+    title: "The 2 of Cups",
+    caption: "The Virtual reality wedding planner represents balance, harmony, and stability in the emotional and relational aspects of life. This card may indicate that it's time to find balance and harmony in your relationships and emotions. Use the virtual reality wedding planner to plan the perfect wedding, and experience the harmony and balance in your life."
   },
   // Add the rest of the images with their captions here.
 ];
@@ -19,9 +21,12 @@ const images = [
 function App() {
   const [text, setText] = useState("");
   const [randomImage, setRandomImage] = useState(null);
+  const [inputWidth, setInputWidth] = useState("60vw");
+  const [inputPlaceholder, setInputPlaceholder] = useState("ask a question about your present");
 
   const handleTextChange = (event) => {
     setText(event.target.value);
+    setInputWidth(`${Math.max(8, event.target.value.length) + 1}ch`);
   };
 
   const selectRandomImage = () => {
@@ -36,15 +41,18 @@ function App() {
   const handleResetClick = () => {
     setText("");
     setRandomImage(null);
+    setInputWidth("60vw");
+    setInputPlaceholder("ask another question about your present");
   };
 
   if (randomImage) {
     return (
-      <div className="button-container">
-        <img src={randomImage.src} alt={randomImage.alt} style={{ width: "60vw" }}/>
+      <div className="container">
+        <img src={randomImage.src} alt={randomImage.alt} style={{ width: "50vw" }}/>
+        <h3>{randomImage.title}</h3>
         <p>{randomImage.caption}</p>
         <div className="button-container">
-          <button onClick={handleResetClick}>  ask another question  </button>
+          <button onClick={handleResetClick}>ask another question</button>
         </div>
       </div>
     );
@@ -52,16 +60,16 @@ function App() {
 
   return (
     <div className="button-container">
-      <input 
-      type="text" 
-      value={text} 
-      onChange={handleTextChange} 
-      placeholder="ask a question about your present"
-      style={{ textAlign: "center" }} 
+      <input
+        type="text"
+        value={text}
+        onChange={handleTextChange}
+        placeholder={inputPlaceholder}
+        style={{ width: inputWidth, fontSize: `${Math.min(1.5, 1.5 - 0.01 * text.length)}em` }}
       />
 
       <div className="button-container">
-        <button onClick={handleButtonClick}>  Ask  </button>
+        <button onClick={handleButtonClick}>Ask</button>
       </div>
     </div>
   );
