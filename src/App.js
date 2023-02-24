64617ae690837edde5818619b991a950a391cd1d
Dave Greber <davegreber@gmail.com>
2023-02-24 10:39:59 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const images = [
+  {
+    name: "tarot_cups_9.jpg",
+    src: "/images/tarot_cups_9.jpg",
+    alt: "Cups 9",
+    caption: "This is the caption for Cups 9."
+  },
+  {
+    name: "tarot_cups_2.jpg",
+    src: "/images/tarot_cups_2.jpg",
+    alt: "Cups 2",
+    caption: "This is the caption for Cups 2."
+  },
+  // Add the rest of the images with their captions here.
+];
 
 function App() {
+  const [text, setText] = useState("");
+  const [randomImage, setRandomImage] = useState(null);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const selectRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+  };
+
+  const handleButtonClick = () => {
+    setRandomImage(selectRandomImage());
+  };
+
+  const handleResetClick = () => {
+    setText("");
+    setRandomImage(null);
+  };
+
+  if (randomImage) {
+    return (
+      <div>
+        <img src={randomImage.src} alt={randomImage.alt} />
+        <p>{randomImage.caption}</p>
+        <button onClick={handleResetClick}>Reset</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={text} onChange={handleTextChange} />
+      <button onClick={handleButtonClick}>Select Random Image</button>
     </div>
   );
 }
